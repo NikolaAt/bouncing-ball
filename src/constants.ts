4afe74dd5ff7879c0bf8fac1c44eabed5ac055c2
NikolaAt <nikolaat1@gmail.com>
2024-02-14 11:02:43 +0200

    Update constants.ts

--- src/constants.ts
+++ src/constants.ts
@@ -1,6 +1,6 @@
 import { KameleoonUtils } from "@kameleoon/javascript-sdk";
 
 // -- Constants values
-export const siteCode = "6h7osatsjh";
+export const siteCode = "cqz89i9wq0";
 export const featureKey = "bouncing_ball_demo";
 export const visitorCode = KameleoonUtils.getVisitorCode("www.example.com");
